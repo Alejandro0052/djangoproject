081e5f664151a37c5fd1246cc085da59b0af6bc6
Alejandro <mariaeugenialopez456@mail.com>
2024-06-15 22:56:40 -0500

    Usando los jinga para crear iteraciones en las listas y diccionarios de las diferentes vistas de la aplicacion

--- myapp/views.py
+++ myapp/views.py
@@ -34,5 +34,8 @@ def projects(request):
 
 def tasks(request):
  #   task = Task.objects.get(title=title)
-    return render(request,'tasks.html')
+    tasks = Task.objects.all()
+    return render(request,'tasks.html', {
+            'tasks':tasks
+    })
 
