91f14b5ca17cdac4312f24ebadede5aa676e962d
Alejandro <mariaeugenialopez456@mail.com>
2024-06-16 22:56:58 -0500

    Agregando un formulario que guarda los datos, en base de datos. los datos qu guarda por ahora desde el formulario son las nuevas tareas, pronto agregare lo mismo para que guarde los projects

---
+++ myapp/forms.py
@@ -0,0 +1,6 @@
+from django import forms 
+
+class CreateNewTask(forms.Form):
+    title = forms.CharField(label="Titulo de tarea", max_length=200)
+    description = forms.CharField(label="Descripcion de la tarea",
+        widget=forms.Textarea)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 #from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import  render, redirect
+from .forms import CreateNewTask
 
 from django.shortcuts import render
 # Create your views here.
@@ -40,4 +41,10 @@ def tasks(request):
     })
 
 def create_task(request):
-    return render(request, 'create_task.html')
+    if request.method == 'GET':
+       return render(request, 'create_task.html', {
+       'form': CreateNewTask()})
+    else:
+        Task.objects.create(title=request.POST['title'],
+        description=request.POST['description'], project_id=2)
+        return redirect('/tasks/')
