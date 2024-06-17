9f75e11dcc45ceb1b5800226b28db439e7492f96
Alejandro <mariaeugenialopez456@mail.com>
2024-06-16 19:12:18 -0500

    Agregando nuevos archivos, dandole orden a la navegacion de la aplicacion para evitar las navegaciones desde la url, jinga ha sido muy util

--- myapp/models.py
+++ myapp/models.py
@@ -12,6 +12,7 @@ class Task(models.Model):
         title = models.CharField(max_length=200)
         description = models.TextField()
         project = models.ForeignKey(Project, on_delete=models.CASCADE)
+        done = models.BooleanField(default=False)
 
         def __str__(self):
                 return self.title + ' - ' + self.project.name
